486c2d4006390768d0f2b048e567f4eef8bf09f0
kalium <jehansingh.97@gmail.com>
2018-01-24 10:46:51 +0200

    Reorganise and create FingerprintEntry Class

--- app/src/main/java/za/co/barclays/strmap/Classifier.java
+++ app/src/main/java/za/co/barclays/strmap/Classifier.java
@@ -5,37 +5,10 @@ import java.util.List;
 
 public class Classifier {
 
-    List<String> fingerprint = new ArrayList<>();
-
-    public float scoreMac(int max, int min, int str){
-
-        float avg = (min+max)/2;
-        float range = Math.abs(max - min);
-        range *= 1.2;
-
-        float score = 0;
-
-        if ((str <= avg-(range/2)) || (str >= avg+(range/2))){
-
-            score = 0;
-
-        }
-        else if (str>=min && str<=max && str != avg){
-
-            score = 1-(Math.abs(str-avg)/(range/2));
-
-        }
-        else if (str == avg){
-            score = 1;
-        }
-
-        return score;
-
-    }
+    List<FingerprintEntry> fingerprint = new ArrayList<>();
 
     public void setFingerprint(ArrayList<String> list){
 
-        fingerprint = list;
 
     }
 
