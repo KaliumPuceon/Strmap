c801b86896c777b1df92f5596bb4aaea7683676f
kalium <jehansingh.97@gmail.com>
2018-01-24 11:23:04 +0200

    Shuffle classifier & move to dedicted scan objects

--- app/src/main/java/za/co/barclays/strmap/Classifier.java
+++ app/src/main/java/za/co/barclays/strmap/Classifier.java
@@ -1,5 +1,9 @@
 package za.co.barclays.strmap;
 
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,14 +11,26 @@ public class Classifier {
 
     List<FingerprintEntry> fingerprint = new ArrayList<>();
 
+    public List<ScanResult> getScan (Context appContext){
+
+        WifiManager wifi = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
+        List<ScanResult> results;
+        wifi.startScan();
+
+        results = wifi.getScanResults();
+
+        return results;
+
+    }
+
     public void setFingerprint(ArrayList<String> list){
 
 
     }
 
-    public float getWholeScore(ArrayList<String> reading){
+    public float getWholeScore(ArrayList<ScanResult> scan){
 
-        for (int k=0;k<reading.size();k++) {
+        for (int k=0;k<scan.size();k++) {
 
 
 
